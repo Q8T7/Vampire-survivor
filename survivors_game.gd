b42f45256d8382260550049899291e56db4a3a1d
Q8T7 <Dalindsey1@s.sfusd.edu>
2024-05-30 09:38:07 -0700

    adding start menu

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var asp = $AudioStreamPlayer
+
+var volume = 8
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
@@ -15,3 +18,11 @@ func _on_timer_timeout():
 func _on_player_health_depleted():
 	%GameOver.visible = true
 	get_tree().paused = true
+
+func _ready():
+	AudioServer.set_bus_volume_db(0,volume)
+	asp.play()
+
+
+func _on_audio_stream_player_finished():
+	asp.play()
